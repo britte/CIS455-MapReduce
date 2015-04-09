ff7965ef2efff0700b3ceefa0a0fa2454fa2506b
britte <britte@github.com>
2015-04-08 22:30:16 -0400

    Basic http client

---
+++ src/edu/upenn/cis455/httpclient/HttpClient.java
@@ -0,0 +1,82 @@
+package edu.upenn.cis455.httpclient;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class HttpClient {
+	
+	public HttpClient() {}
+
+	// Create a connection to the given url 
+	public int sendRequest(HttpRequest req) throws IOException {
+		if (req.getMethod().equals("GET")) {
+			return this.sendGet(req);
+		} else if (req.getMethod().equals("POST")) {
+			return this.sendPost(req);
+		} else {
+			return -1;
+		}
+	}
+	
+	public int sendGet(HttpRequest req) {
+		try {
+			// Establish connection to url (with params in query string)
+			URL url = new URL(req.getBaseUrl() + "?" + req.getEncodedParams());
+			HttpURLConnection conn;
+			if (url.getProtocol().equals("http")) {
+				conn = (HttpURLConnection) url.openConnection();
+			} else if (url.getProtocol().equals("https")){
+				conn = (HttpsURLConnection) url.openConnection();
+			} else {
+				// Invalid connection
+				return HttpURLConnection.HTTP_BAD_REQUEST;
+			}
+			
+			conn.setRequestMethod("GET");
+			conn.connect();
+			int status = conn.getResponseCode();
+			conn.disconnect();
+			return status;
+			
+		} catch (Exception e){
+			e.printStackTrace();
+			return HttpURLConnection.HTTP_BAD_REQUEST;
+		}
+	}
+	
+	public int sendPost(HttpRequest req) {
+		try {
+			// Establish connection to base url
+			URL url = new URL(req.getBaseUrl());
+			HttpURLConnection conn;
+			if (url.getProtocol().equals("http")) {
+				conn = (HttpURLConnection) url.openConnection();
+			} else if (url.getProtocol().equals("https")){
+				conn = (HttpsURLConnection) url.openConnection();
+			} else {
+				// Invalid connection
+				return HttpURLConnection.HTTP_BAD_REQUEST;
+			}
+			
+			conn.setDoOutput(true); // sets POST
+			
+			// Send params as a body
+			PrintWriter out = new PrintWriter(new BufferedOutputStream(conn.getOutputStream()));	
+		    out.write(req.getEncodedParams());
+		    
+			int status = conn.getResponseCode();
+			conn.disconnect();
+			return status;
+			
+		} catch (Exception e){
+			e.printStackTrace();
+			return HttpURLConnection.HTTP_BAD_REQUEST;
+		}
+	}
+
+}
---
+++ src/edu/upenn/cis455/httpclient/HttpRequest.java
@@ -0,0 +1,45 @@
+package edu.upenn.cis455.httpclient;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+public class HttpRequest {
+	
+	private String baseUrl;
+	private String method; 
+	private HashMap<String, HashSet<String>> params = new HashMap<String, HashSet<String>>();
+	
+	public HttpRequest(String baseUrl, String method){
+		this.baseUrl = baseUrl;
+		this.method = method.toUpperCase();
+	}
+	
+	public void setParam(String name, String value) {
+		HashSet<String> vals = this.params.get(name);
+		if (vals == null) vals = new HashSet<String>();
+		
+		vals.add(value);
+		
+		this.params.put(name, vals);
+	}
+	
+	public String getBaseUrl() { return this.baseUrl; }
+	public String getMethod() { return this.method; }
+	public String getEncodedParams() throws UnsupportedEncodingException { 
+		StringBuilder pString = new StringBuilder();
+		Iterator<Entry<String, HashSet<String>>> i = params.entrySet().iterator();
+		while (i.hasNext()) {
+			Entry<String, HashSet<String>> param = i.next();
+			for (String value : param.getValue()) {
+				String encodedKey = URLEncoder.encode(param.getKey(), "UTF-8");
+				String encodedVal = URLEncoder.encode(value, "UTF-8");
+				pString.append(encodedKey + "=" + encodedVal + "&");
+			}
+		}
+		return pString.substring(0, pString.length() - 1);
+	}
+}
